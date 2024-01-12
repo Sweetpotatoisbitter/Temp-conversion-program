8124df7e3148321b6fa89f79aafac4bcabf19e55
Aniket <singhaniketkumar108@gmail.com>
2024-01-12 20:24:20 +0530

    updated  basic javascript

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const textbox = document.getElementById("textbox");
+const tofahrenheit = document.getElementById("tofahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
 function convert(){
+    if(tofahrenheit.checked){
+        temp = Number(textbox.value);
+        temp= temp * 9 / 5 + 32;
+        result.textContent = "temp. is  "+  temp.toFixed(1)  + "°F✌️"
+
+    }else if(toCelsius.checked){
+        temp = Number(textbox.value);
+        temp = (temp-32) * (5/9);
+
+        result.textContent = "temp. is  "+ temp.toFixed(1)  + "°C👍"
+
+    }else{
+        result.textContent = "Select a unit,dawg!😎"
+    }
 
 }
+
+
+
+
